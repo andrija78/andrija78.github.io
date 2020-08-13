ef5eb2e76b9d70285f9710a0f766ea60aad67954
andrija78 <andrija.rubelj@gmail.com>
2020-08-13 10:28:16 +0400

    cleanup

--- mcts_js/worker_bundle.js
+++
@@ -1,9 +0,0 @@
-//import mcts from './index'; //for debugging only
-
-onmessage = function(event) {
-
-  //console.log('Hello from the worker.');
-  let message=mcts(event);
-  postMessage(message);
-
-}
