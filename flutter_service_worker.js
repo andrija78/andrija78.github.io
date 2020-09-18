c5f365cfdf4c6aa54adc348af143349ecff7aa66
andrija78 <andrija.rubelj@gmail.com>
2020-09-18 08:59:42 +0400

    0.6.3+2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,9 +55,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "9a191e182229e9270c74daac80d592f6",
-"/": "9a191e182229e9270c74daac80d592f6",
-"main.dart.js": "3b9afe49f2fec8346e5c6d0b7048dc95",
+"index.html": "b727ace37b4434b95405b5f409943860",
+"/": "b727ace37b4434b95405b5f409943860",
+"main.dart.js": "1a4ef62278eba960ed49de771ce038b8",
 "manifest.json": "34ce20b934ade9c43d4c1a9140614d9b",
 "mcts_js/main.bundle.js": "3f778d0f791c70abac917eb762cc8228"
 };
